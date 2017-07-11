2514633a01ace9f8ea3414d3250fd759765b1791
Vince Carey <stvjc@channing.harvard.edu>
2017-07-11 16:37:57 -0400

    switched to packageStartupMessage

--- R/zzz.R
+++ R/zzz.R
@@ -6,10 +6,10 @@
 #' @export h5py
 #' @export sk
 .onLoad = function(libname, pkgname) {
-  message("Setting up python infrastructure, including numpy with convert=FALSE ...")
+  packageStartupMessage("Setting up python infrastructure, including numpy with convert=FALSE ...")
   np <<- import("numpy", delay_load=TRUE, convert=FALSE)
   pd <<- import("pandas", delay_load=TRUE)
   h5py <<- import("h5py", delay_load=TRUE)
   sk <<- import("sklearn.decomposition", delay_load=TRUE)
-  message("Done.")
+  packageStartupMessage("Done.")
 }
